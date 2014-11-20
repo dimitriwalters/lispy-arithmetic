1c8a5cf2772ae4abf70dd39cfa6a9233b222f239
Dimitri Walters <me@dimitriwalters.com>
2014-11-19 20:30:20 -0500

    Add arithmetic methods

--- lispy-arithmetic.js
+++ lispy-arithmetic.js
@@ -9,5 +9,53 @@
         root.la = lispyArithmetic();
     }
 }(this, function () {
-    return {};
+    function isEmpty (arr) {
+        return arr.length === 0;
+    }
+
+    function first (arr) {
+        return arr[0];
+    }
+
+    function rest (arr) {
+        return Array.prototype.slice.call(arr, 1);
+    }
+
+    function sum (arr) {
+        return Array.prototype.reduce.call(arr, function (sum, num) {
+            return sum + num;
+        }, 0);
+    }
+
+    function prod (arr) {
+        return Array.prototype.reduce.call(arr, function (prod, num) {
+            return prod * num;
+        }, 1);
+    }
+
+    return {
+        add: function () {
+            return sum(arguments);
+        },
+
+        sub: function () {
+            if (isEmpty(arguments)) {
+                return 0;
+            } else {
+                return first(arguments) - sum(rest(arguments));
+            }
+        },
+
+        mult: function () {
+            return prod(arguments);
+        },
+
+        div: function () {
+            if (isEmpty(arguments)) {
+                return 1;
+            } else {
+                return first(arguments) / prod(rest(arguments));
+            }
+        }
+    };
 }));
